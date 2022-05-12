68a40f3b4a2135b34ff054105d617ca2ad40c7ef
gvfork <gvidal@betterfood.cl>
2022-05-12 14:44:42 -0400

    newest

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
   name = 'foopackage',         # How you named your package folder (MyLib)
   packages = ['foopackage'],   # Chose the same as "name"
   version = '0.4',      # Start with a small number and increase it with every change you make
-  license = 'GPLv2',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  license = 'Free for non-commercial use',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'This is a test package',   # Give a short description about your library
   author = 'Gonzalo Vidal',                   # Type in your name
   author_email = 'gvidal@betterfood.com',      # Type in your E-Mail
@@ -18,7 +18,7 @@ setup(
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',   # Again, pick a license
+    'License :: Free for non-commercial use',   # Again, pick a license
     'Programming Language :: Python :: 3.10',      #Specify which pyhton versions that you want to support
   ],
 )
