b3daac530b49188e04dfba9ad2c3be014fea42b6
gvfork <gvidal@betterfood.cl>
2022-05-12 14:45:14 -0400

    newest

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'foopackage',         # How you named your package folder (MyLib)
   packages = ['foopackage'],   # Chose the same as "name"
-  version = '0.4',      # Start with a small number and increase it with every change you make
+  version = '0.5',      # Start with a small number and increase it with every change you make
   license = 'Free for non-commercial use',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'This is a test package',   # Give a short description about your library
   author = 'Gonzalo Vidal',                   # Type in your name
   author_email = 'gvidal@betterfood.com',      # Type in your E-Mail
   url = 'https://github.com/gvfork/pypiserver-test',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/gvfork/pypiserver-test/archive/refs/tags/v0.4.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/gvfork/pypiserver-test/archive/refs/tags/v0.5.tar.gz',    # I explain this later on
   keywords = ['foo', 'bar'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
